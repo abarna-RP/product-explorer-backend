c188152ef90d68b45d7ae0e22db1107660d55dad
abarna-RP <prabaabu62@gmail.com>
2025-09-23 21:06:08 +0530

    Fixed port issue on Render deployment

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // ✅ Allow requests from frontend (http://localhost:3000)
+  // ✅ Allow requests from frontend
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3000', // Production-ல் உங்கள் Vercel URL-ஐ இங்கே கொடுக்க வேண்டும்
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  await app.listen(3001); // backend port
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
